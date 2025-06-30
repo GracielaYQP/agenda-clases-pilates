4804cdccc3b8404bf41246977e2c7fe394319563
Graciela YQP <gratheiler@gmail.com>
2025-06-30 20:40:32 -0300

    Mis cambios

---
+++ Frontend/src/app/clases/clases.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ParallaxComponent } from '../shared/parallax/parallax.component';
+
+@Component({
+  selector: 'app-clases',
+  standalone: true,
+  imports: [ParallaxComponent],
+  templateUrl: './clases.component.html',
+  styleUrl: './clases.component.css'
+})
+export class ClasesComponent {
+
+}
---
+++ Frontend/src/app/horarios-disponibles/horarios-disponibles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horarios-disponibles',
+  standalone: true,
+  imports: [],
+  templateUrl: './horarios-disponibles.component.html',
+  styleUrl: './horarios-disponibles.component.css'
+})
+export class HorariosDisponiblesComponent {
+
+}
